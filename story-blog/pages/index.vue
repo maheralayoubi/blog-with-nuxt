a19fc6f51fd76bd78f4e5ce5db0baca6eee9638f
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-10-30 12:19:15 +0900

    Creating article data

--- story-blog/pages/index.vue
+++ story-blog/pages/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="container">
-
+    <nuxt-link
+      v-for="post in posts"
+      :key="post.id"
+      :to="post.id">
+      <article class="post-preview"></article>
+    </nuxt-link>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      posts: [
+        {
+        title: 'A New Idea',
+        previewText: 'This will be great',
+        thumbnailUrl: 'http://cfjp.xsrv.jp/ess-demo/wp-content/uploads/2017/03/portfolio5.jpg',
+        id: 'a-new-beginning'
+        },
+        {
+          title: 'A Second Idea',
+          previewText: 'This will be great',
+          thumbnailUrl: 'http://cfjp.xsrv.jp/ess-demo/wp-content/uploads/2017/03/portfolio5.jpg',
+          id: 'a-second-beginning'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
