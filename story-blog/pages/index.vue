b0c8a3c851bd16bb1691fa449a8f047d34547c6b
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-11-03 23:05:24 +0900

    Renderig the article properties in the template

--- story-blog/pages/index.vue
+++ story-blog/pages/index.vue
@@ -4,7 +4,16 @@
       v-for="post in posts"
       :key="post.id"
       :to="post.id">
-      <article class="post-preview"></article>
+      <article class="post-preview">
+        <div
+          :style="{backgroundImage:post.thumbnailUrl}"
+          class="post-preview-thumbnail">
+        </div>
+        <div class="post-preview-content">
+          <h1>{{ post.title }}</h1>
+          <p>{{ post.previewText }}</p>
+        </div>
+      </article>
     </nuxt-link>
   </div>
 </template>
