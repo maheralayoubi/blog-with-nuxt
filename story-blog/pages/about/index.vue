4354d131e553c9b10522a294164e8c18ee713a56
Maher <maher91syr@gmail.com>
2021-01-29 00:32:50 +0900

    Creating asyncData for about blog

--- story-blog/pages/about/index.vue
+++ story-blog/pages/about/index.vue
@@ -1,6 +1,22 @@
 <template>
   <section id="about-page">
-    <h1>Hi There!</h1>
-    <p>Had you in my pocked</p>
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
   </section>
 </template>
+
+<script>
+export default {
+  asyncData(context) {
+    return context.app.$storyapi.get('cdn/stories/about', {
+      version: 'draft'
+    }).then(res => {
+      console.log(res.data)
+      return {
+        title: res.data.story.content.title,
+        content: res.data.story.content.content
+      }
+    })
+  }
+}
+</script>
